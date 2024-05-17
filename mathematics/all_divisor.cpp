88eebee32bb818eadc8c8aba24e5aad27a64a9fa
ManashKumar_0723 <manashshri723@gmail.com>
2024-05-17 23:24:24 +0530

    all_divisor

---
+++ mathematics/all_divisor.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+void alldivisor(int n){
+    int i;
+    for(int i=1;i<=sqrt(n);i++){ //for first divisor for 1 to rot n
+        if(n%i==0)
+        cout<<i<<" ";}
+    for( ; i>=1;i--){
+        if(n%i==0)
+       
+        cout<<(n/i)<<" "; }
+    
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<"all_divisorsare";
+    alldivisor(n);
+    return 0;
+}
+
