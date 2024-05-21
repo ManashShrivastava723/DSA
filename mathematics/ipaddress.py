37c9b4b225f12a2aab54d24a2696ee6c40d8016e
ManashKumar_0723 <manashshri723@gmail.com>
2024-05-22 01:22:45 +0530

    ipaddress.py

--- mathematics/ipaddress.py
+++ mathematics/ipaddress.py
@@ -1,16 +1,16 @@
 def validateipaddress(ip):
-    number = ip.split('.')
+    number = ip.split('.') 
     if len(number) != 4: #4 part hona chaiye kisi ip address ka
-        print(f'Invalid IP address: {ip}')
+        print(f'Invalid IP address: {ip}') 
         return
-    for part in number:
-        if not part.isdigit(): #sara part number
+    for part in number: 
+        if not part.isdigit():  #sara part number
             print(f'Invalid IP address: {ip}')
             return
         num = int(part) #to check range we are converting them
         if num < 0 or num > 255: 
             print(f'Invalid IP address: {ip}')
-            return
+            return 
         if part != "0" and part.startswith("0") and part.ends: #zero checking starting me nhi ho agar whole part is not zero then
             print(f'Invalid IP address: {ip}')
             return
@@ -20,4 +20,4 @@ while True:
     our_input = input("Enter an IPv4 address if want to exit write exit ")
     if our_input.upper() == 'EXIT':
         break
-    validateipaddress(our_input)
+    validateipaddress(our_input)   
