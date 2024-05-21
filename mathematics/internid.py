db4700d29086148fd8da9185005a568d1d0e6510
ManashKumar_0723 <manashshri723@gmail.com>
2024-05-21 22:00:57 +0530

    internid.py

---
+++ mathematics/internid.py
@@ -0,0 +1,20 @@
+id=int(input("enter id"))
+n = list(range(1,25))
+internid = []
+pos = -1
+for i in n:
+    j = 1
+    while j <= i:
+        if j == 1:
+            internid.append(5000*i)
+        else:
+            internid.append(internid[j-1]+5000+(j-1))
+        pos += 1
+        
+        if id == internid[pos]:
+            print(f'Intern {i}')
+            print(f'Day {j}')
+            
+        j += 1
+        
+    i += 1
